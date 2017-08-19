605648b7c63dba9054b67c7d8c36b4695e444f74
Feroze Daud <ferozed@zillow.com>
2017-08-18 19:24:10 -0700

    initial checkin

---
+++ zquick-agent/src/test/java/com/zillow/helloworld/HelloWorldTest.java
@@ -0,0 +1,20 @@
+package com.zillow.helloworld;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+    private HelloWorld helloWorld;
+
+    @Before
+    public void setup() {
+        helloWorld = new HelloWorld();
+    }
+
+    @Test
+    public void testHelloWorld() {
+        Assert.assertNotNull(helloWorld.hello());
+    }
+}
---
+++ zquick-example/src/main/java/com/zillow/helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.zillow.helloworld;
+
+public class HelloWorld {
+
+    public String hello() {
+        return "Hello from the zquick-example jar!";
+    }
+}
